ece773fc06f55ee21fc0bdf2ea3ee91ec16e8f54
Mathteus <85768227+Mathteus@users.noreply.github.com>
2021-07-13 19:59:24 -0300

    v1

---
+++ Cliente.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cadastroClientes
+{
+    class Cliente
+    {
+        public string nome;
+        public string telefone;
+        public string cpf;
+
+        /*public static List<Cliente> lerClientes()
+        {
+            var clientes = new List<Cliente>(); 
+            return clientes;
+        }*/
+
+        public void gravar()
+        {
+
+        }
+
+        static List<Cliente> clientes = new List<Cliente>();
+        static StreamReader leitor = new StreamReader(caminhoDBCliente());
+        //StreamWriter escritor = new(caminhoDBCliente());
+
+        public static string caminhoDBCliente()
+        {
+            string caminho = @"C:\Users\Shadow\Documents\things progaming\c#\arquivo\file1.txt";
+            return caminho;
+        }
+
+        public static List<Cliente> lerClientes()
+        {
+            if (File.Exists(caminhoDBCliente()))
+            {
+                using (StreamReader leitor = new StreamReader(caminhoDBCliente()))
+                {
+                    string linha;
+                    int i = 0;
+                    while ((linha = leitor.ReadLine()) != null)
+                    {
+                        i++;
+                        if (i > 1)
+                        {
+                            var clienteArquivo = linha.Split(';');
+                            Cliente cliente = new Cliente();
+
+                            cliente.nome = clienteArquivo[0];
+                            cliente.telefone = clienteArquivo[1];
+                            cliente.cpf = clienteArquivo[2];
+                            clientes.Add(cliente);
+                        }
+
+                    }
+                }
+            }
+            return clientes;
+        }
+
+        public static void verClientes()
+        { 
+            int id;
+            int cont = clientes.Count();
+            for (id = 0; id < cont; id++)
+            {
+                Console.WriteLine("Cliente {0}", id);
+                Console.WriteLine("Nome......: {0}", clientes[id].nome);
+                Console.WriteLine("Telefone..: {0}", clientes[id].telefone);
+                Console.WriteLine("CPF.......: {0}", clientes[id].cpf);
+            }
+        }
+
+        public static void fecharInputs()
+        {
+            leitor.Close();
+            //escritor.Close();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System;
+using System.Threading.Tasks;
+
+namespace cadastroClientes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente.lerClientes();
+
+            Cliente jonisvaldo = new Cliente();
+            jonisvaldo.nome = "Jonisvaldo";
+            jonisvaldo.telefone = "36221994";
+            jonisvaldo.cpf = "49481828859";
+            jonisvaldo.gravar();
+
+            Cliente creuza = new Cliente();
+            creuza.nome = "Creuza";
+            creuza.telefone = "12445796";
+            creuza.cpf = "21543487";
+            creuza.gravar();
+
+            Cliente.verClientes();
+            Cliente.fecharInputs();
+            Console.Read();
+
+        }
+        
+    }
+}
